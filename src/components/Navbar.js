eb03af1e3e7db04f93bcfe1d584cd5c68d487a3e
shailly <shaillyag2003@gmail.com>
2024-11-14 18:36:11 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css';
 function Navbar() {
     return (
         <nav className="navbar">
-            <div className="logo">S.</div>
+            <div className="logo">I.</div>
             <ul className="nav-links">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
